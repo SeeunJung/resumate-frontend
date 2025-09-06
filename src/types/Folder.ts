83173e1a24fac179a4753801c26e74ac25ad2440
Seeun Jung <seeun.jung01@gmail.com>
2025-09-07 00:16:26 +0900

    feat: 폴더 관련 api 호출 함수 정의

--- src/types/Folder.ts
+++ src/types/Folder.ts
@@ -1,13 +1,11 @@
 export type Folder = {
-  id: bigint
-  parent_id: bigint | null
-  member_id: bigint
-  name: string
-  order: number
-  created_at: string
-  modified_at: string
-  deleted_at: string | null
-  is_deleted: boolean
+  id?: number
+  parentId?: number | null
+  parentName?: string | null
+  name?: string
+  order?: number
+  modifiedAt?: string
+  children?: Folder[]
 }
 
 export type FolderNode = Folder & {
