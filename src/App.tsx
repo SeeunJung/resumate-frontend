302d3444085f4b16846dc4eddb0e1050524a0b09
Seeun Jung <seeun.jung01@gmail.com>
2025-09-11 13:35:54 +0900

    fix: 메인페이지 requireAuth 상태 변경

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
           <Route
             path="/"
             element={
-              <AuthWrapper requireAuth={false}>
+              <AuthWrapper requireAuth={true}>
                 <Home />
               </AuthWrapper>
             }
